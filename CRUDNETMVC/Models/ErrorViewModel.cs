de88a9eca6b0f869f66c37607cbfce25179e248d
Juan Pablo Ponce <jppb1982@gmail.com>
2024-04-08 12:20:12 -0300

    Primer commit -Comienzo a trabajar vista de 'Lista de contactos'

---
+++ CRUDNETMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDNETMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
