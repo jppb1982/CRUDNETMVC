6976313f4d032c99a34fb088ebcb037fb3ef2b0f
Juan Pablo Ponce <jppb1982@gmail.com>
2024-04-08 15:07:39 -0300

    Primer commit de cero hasta comenzar a trabajar en la vista de 'Lista de contactos'

---
+++ CRUDNETMVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUDNETMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDNETMVC.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //Agregar los modelos de los aquí (Cada modelo corresponde a una tabla en BD)
+        public DbSet<Contacto> Contacto { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
