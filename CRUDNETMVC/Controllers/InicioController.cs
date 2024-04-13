54eda9f786681debd962d0c43bf2ee756ef57c47
Juan Pablo Ponce <jppb1982@gmail.com>
2024-04-13 12:43:22 -0300

    Crear y Editar contacto completo

--- CRUDNETMVC/Controllers/InicioController.cs
+++ CRUDNETMVC/Controllers/InicioController.cs
@@ -12,14 +12,72 @@ namespace CRUDNETMVC.Controllers
 
         public InicioController(ApplicationDbContext contexto)
         {
-            _context = contexto;    
+            _context = contexto;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Contacto.ToListAsync());
         }
 
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            //Valida de que se cumplan las condiciones declaradas en el modelo de datos.
+            //En este caso se valida que los campos sean requeridos
+            if (ModelState.IsValid)
+            {
+                //Agregar fecha y horqa actual
+                contacto.FechaCreacion = DateTime.Now;
+
+                _context.Contacto.Add(contacto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _context.Contacto.Find(id);
+
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            //Valida de que se cumplan las condiciones declaradas en el modelo de datos.
+            //En este caso se valida que los campos sean requeridos
+            if (ModelState.IsValid)
+            {
+                _context.Update(contacto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
